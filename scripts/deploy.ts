a33c4e10f6ac92878a36b5c798787a93a35b9229
Pavan Chindukuri <chindukuripavan@gmail.com>
2022-08-28 19:40:23 +0530

    adding seeding info

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,47 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-	// Fetch Contract
+	console.log('Deploying contract...\n');
+
+	// Fetch Contracts
 	const Token = await ethers.getContractFactory('Token');
+	const Exchange = await ethers.getContractFactory('Exchange');
+	// Get some accounts
+	const accounts = await ethers.getSigners();
+	// 1. accounts[0] is the deployer
+	// 2. accounts[1] is the feeAccount
+	console.log(
+		`Accounts fetched:\n ${accounts[0].address}\n ${accounts[1].address}\n ${accounts[2].address}\n`,
+	);
+
 	// Deploy Contract
-	const token = await Token.deploy(
+	const csToken = await Token.deploy(
 		'CSToken',
 		'CS',
 		ethers.utils.parseEther('1000000'),
 	);
-	await token.deployed();
-	console.log(token.address);
+	await csToken.deployed();
+	console.log(`CSToken deployed at: ${csToken.address}\n`);
+
+	const mETH = await Token.deploy(
+		'mETH',
+		'mETH',
+		ethers.utils.parseEther('1000000'),
+	);
+	await mETH.deployed();
+	console.log(`mETH Deployed to: ${mETH.address}`);
+
+	const mDAI = await Token.deploy(
+		'mDAI',
+		'mDAI',
+		ethers.utils.parseEther('1000000'),
+	);
+	await mDAI.deployed();
+	console.log(`mDAI Deployed to: ${mDAI.address}`);
+
+	const exchange = await Exchange.deploy(accounts[1].address, 10);
+	await exchange.deployed();
+	console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
 main().catch(error => {
